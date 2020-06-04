ce7032811f277a116d6877cd5a538fa2db1939b3
laoqin <694170828@qq.com>
2020-06-04 13:33:48 +0800

    删除

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import { getBanner } from '../service/home';
-import axios from 'axios';
-const CancelToken = axios.CancelToken;
-const source = CancelToken.source();
 
 class About extends Component {
-  componentDidMount() {
-    getBanner({}).then((res) => {
-      console.log(res);
-    });
-  }
   render() {
     return <>About</>;
   }
--- src/pages/Mine.jsx
+++ src/pages/Mine.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { getError } from '../service/home';
+
 class Mine extends React.Component {
   render() {
-    return <>Mine 演示请求 token 到期， 需要跳转到首页</>;
+    return <>Mine 页</>;
   }
 }
 export default Mine;
